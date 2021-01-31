72d8280b06ae57d2b697e0c0b9ba2426326538a1
Mike Hopkins <michael.l.hopkins@gmail.com>
2021-01-31 09:26:53 -0600

    Arcade Drive with Twist only turning. NavXP Throttle control of speed on driver joystick and twist control on operator joystick

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,269 +1,138 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 #include "Robot.h"
 
-#include <frc/Joystick.h>
-#include <frc/PWMVictorSPX.h>
-#include <frc/TimedRobot.h>
-#include <frc/drive/DifferentialDrive.h>
 
-#include <iostream>
+double Robot::turning;
+double Robot::currentDistance;
 
-#include <frc/smartdashboard/SmartDashboard.h>
+void Robot::RobotInit()
+{
+  // Init Timer
+  m_timer.Start();
 
-using namespace frc;
-
-void Robot::RobotInit() {
-  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
-  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
-  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-
-  
-try
-  {
-    /***********************************************************************
-     * navX-MXP:
-     * - Communication via RoboRIO MXP (SPI, I2C) and USB.            
-     * - See http://navx-mxp.kauailabs.com/guidance/selecting-an-interface.
-     * 
-     * navX-Micro:
-     * - Communication via I2C (RoboRIO MXP or Onboard) and USB.
-     * - See http://navx-micro.kauailabs.com/guidance/selecting-an-interface.
-     * 
-     * VMX-pi:
-     * - Communication via USB.
-     * - See https://vmx-pi.kauailabs.com/installation/roborio-installation/
-     * 
-     * Multiple navX-model devices on a single robot are supported.
-     ************************************************************************/
-    ahrs = new AHRS(SPI::Port::kMXP);
-  }
-  catch (std::exception &ex)
-  {
-    std::string what_string = ex.what();
-    std::string err_msg("Error instantiating navX MXP:  " + what_string);
-    const char *p_err_msg = err_msg.c_str();
-    DriverStation::ReportError(p_err_msg);
-  }
-
-   // Init Robot Drive
+  // Init Robot Drive
   m_robotDrive.SetExpiration(0.1);
   m_drvL = 0.0;
   m_drvR = 0.0;
 
+// Init Shooter Drive
+#if R2JESU_TURNON_SHOOTER
+  m_ShooterMotorLeft.RestoreFactoryDefaults();
+  m_ShooterMotorRight.RestoreFactoryDefaults();
+  //  Invert the right motor
+  m_ShooterMotorRight.SetInverted(true);
+#endif
+
+// Init Winch Drive
+#if R2JESU_TURNON_WINCH
+  m_winchMotor.Set(0.0);
+#endif
+
+// Init Intake Drive
+#if R2JESU_TURNON_INTAKE
+  snowMotor.Set(0.0);
+#endif
+
+  //  Init Color Sensor
+  m_colorMatcher.AddColorMatch(kBlueTarget);
+  m_colorMatcher.AddColorMatch(kGreenTarget);
+  m_colorMatcher.AddColorMatch(kRedTarget);
+  m_colorMatcher.AddColorMatch(kYellowTarget);
+  m_colorMatcher.AddColorMatch(Default);
+
+  // Run Pneumatics
+#if R2JESU_TURNON_PNEUMATICS
+  // Set Compressor Object for automatic closed loop control
+  compressorObject.SetClosedLoopControl(true);
+  // Set Solenoids to iniital stat
+  ballPopper.Set(false);
+#endif
+
+// NavX Sensor
+#if R2JESU_TURNON_NAV
+  ahrs = new AHRS(frc::SPI::Port::kMXP);
+  ahrs->ZeroYaw();
+#endif
+
+  // Vision & Camera Init
+
+  // Drive USB Camera - 1
+  cs::UsbCamera drvCamera = frc::CameraServer::GetInstance()->StartAutomaticCapture(1);
+  drvCamera.SetResolution(320, 240);
+  drvCamera.SetExposureAuto();
+//  drvCamera.SetFPS(15);
+
+// Vision Processing Camera - 0
+#if R2JESU_TURNON_VISION
+  std::thread visionThread(VisionThread);
+  visionThread.detach();
+#endif
+
 }
 
-/**
- * This function is called every robot packet, no matter the mode. Use
- * this for items like diagnostics that you want ran during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {}
+void Robot::RobotPeriodic(){
+/*frc::SmartDashboard::PutNumber("IMU_Yaw", ahrs->GetYaw());
+frc::SmartDashboard::PutNumber("IMU_Pitch", ahrs->GetPitch());
+frc::SmartDashboard::PutNumber("IMU_Roll", ahrs->GetRoll());
+frc::SmartDashboard::PutNumber("IMU_CompassHeading", ahrs->GetCompassHeading());
+frc::SmartDashboard::PutNumber("IMU_Accel_X", ahrs->GetWorldLinearAccelX());
+frc::SmartDashboard::PutNumber("IMU_Accel_Y", ahrs->GetWorldLinearAccelY());
+frc::SmartDashboard::PutNumber("Displacement_X", ahrs->GetDisplacementX());
+frc::SmartDashboard::PutNumber("Displacement_Y", ahrs->GetDisplacementY());
+frc::SmartDashboard::PutNumber("RawGyro_X", ahrs->GetRawGyroX());
+frc::SmartDashboard::PutNumber("RawGyro_Y", ahrs->GetRawGyroY());
+frc::SmartDashboard::PutNumber("RawGyro_Z", ahrs->GetRawGyroZ()); 
+*/
 
-/**
- * This autonomous (along with the chooser code above) shows how to select
- * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
- *
- * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
- */
-void Robot::AutonomousInit() {
-  m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
+double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
+double targetArea = table->GetNumber("ta",0.0);
+double targetSkew = table->GetNumber("ts",0.0); 
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+frc::SmartDashboard::PutNumber("targetOffsetAngle_Horizontal", table->GetNumber("tx",0.0));
+frc::SmartDashboard::PutNumber("targetOffsetAngle_Vertical", table->GetNumber("ty",0.0));
+frc::SmartDashboard::PutNumber("targetArea", table->GetNumber("ta",0.0));
+frc::SmartDashboard::PutNumber("targetSkew", table->GetNumber("ts",0.0));
+
+//double ourDist = (((double)98.25 - (double)31) / (tan((double)15 + targetOffsetAngle_Vertical)));
+double ourDist = (((double)98.25 - (double)31) / (tan((double)15 + (double)8.8)));
+frc::SmartDashboard::PutNumber("distance", ourDist);
 }
 
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
-}
-
-void Robot::TeleopInit() {}
-
-void Robot::TeleopPeriodic() 
+void Robot::TeleopInit()
 {
-  if (!ahrs)
-    return;
-
-
-  // Reset NavXP
-  if ( m_DrvStickL.GetRawButton(1))
-  {
-    ahrs->ZeroYaw();
-    ahrs->ResetDisplacement ();
-  }
-
-  // Output NavXP Data
-  SmartDashboard::PutBoolean("IMU_Connected", ahrs->IsConnected());
-  SmartDashboard::PutNumber("IMU_Yaw", ahrs->GetYaw());
-  SmartDashboard::PutNumber("IMU_Pitch", ahrs->GetPitch());
-  SmartDashboard::PutNumber("IMU_Roll", ahrs->GetRoll());
-  SmartDashboard::PutNumber("IMU_CompassHeading", ahrs->GetCompassHeading());
- 
-  /* These functions are compatible w/the WPI Gyro Class */
-  SmartDashboard::PutNumber("IMU_TotalYaw", ahrs->GetAngle());
-  SmartDashboard::PutNumber("IMU_YawRateDPS", ahrs->GetRate());
-
-  SmartDashboard::PutNumber("IMU_Accel_X", ahrs->GetWorldLinearAccelX());
-  SmartDashboard::PutNumber("IMU_Accel_Y", ahrs->GetWorldLinearAccelY());
-  SmartDashboard::PutBoolean("IMU_IsMoving", ahrs->IsMoving());
- 
-  SmartDashboard::PutNumber("Velocity_X", ahrs->GetVelocityX());
-  SmartDashboard::PutNumber("Velocity_Y", ahrs->GetVelocityY());
-  SmartDashboard::PutNumber("Displacement_X", ahrs->GetDisplacementX());
-  SmartDashboard::PutNumber("Displacement_Y", ahrs->GetDisplacementY());
-
-  /* Display Raw Gyro/Accelerometer/Magnetometer Values                       */
-  /* NOTE:  These values are not normally necessary, but are made available   */
-  /* for advanced users.  Before using this data, please consider whether     */
-  /* the processed data (see above) will suit your needs.                     */
-
-  SmartDashboard::PutNumber("RawGyro_X", ahrs->GetRawGyroX());
-  SmartDashboard::PutNumber("RawGyro_Y", ahrs->GetRawGyroY());
-  SmartDashboard::PutNumber("RawGyro_Z", ahrs->GetRawGyroZ());
-  SmartDashboard::PutNumber("RawAccel_X", ahrs->GetRawAccelX());
-  SmartDashboard::PutNumber("RawAccel_Y", ahrs->GetRawAccelY());
-  SmartDashboard::PutNumber("RawAccel_Z", ahrs->GetRawAccelZ());
- 
-  // Process Robot Drive
-  R2Jesu_ProcessDrive();
-
+  gameColor = nun;
+  m_ShooterMotorLeft.Set(0.0);
+  m_ShooterMotorRight.Set(0.0);
 }
 
-void Robot::DisabledInit() {}
+void Robot::TeleopPeriodic()
+{
 
-void Robot::DisabledPeriodic() {}
+  // Set the target color
+  R2Jesu_CheckGameTargetColor();
 
-void Robot::TestInit() {}
+  // Process intake motor commands
+  R2Jesu_ProcessIntake();
+
+  // Process user control before drive control.
+  R2Jesu_ProcessUserControl();
+
+  // Note this needs to come last to merge other drive motor request.
+  R2Jesu_ProcessDrive();
+}
 
 void Robot::TestPeriodic() {}
 
-void Robot::R2Jesu_ProcessDrive(double p_LimitFactor)
-{
-
-  double LinX = m_DrvStickL.GetX();
-  double LinY = m_DrvStickL.GetY();
-  double LinT = m_DrvStickL.GetTwist();
-  double RinX = m_DrvStickR.GetX();
-  double RinY = m_DrvStickR.GetY();
-  double RinT = m_DrvStickR.GetTwist();
-
-  double VarTwistLim = 0.0;
-  double curDrvL;
-
-  frc::SmartDashboard::PutNumber("Lx", LinX);
-  frc::SmartDashboard::PutNumber("Ly", LinY);
-  frc::SmartDashboard::PutNumber("Lt", LinT);
-  frc::SmartDashboard::PutNumber("Rx", RinX);
-  frc::SmartDashboard::PutNumber("Ry", RinY);
-  frc::SmartDashboard::PutNumber("Rt", RinT);
-
-//  const double TwistLimtFactor = 0.4;
-  // Values goes -1.0 to 1.0.  So shift by -1.0 to make positive 
-  // and divide by 2.0 to make range 0.0 to 1.0 
-  double PwrLimFactor    = -(m_DrvStickL.GetThrottle()-1.0)/2.0;  // Left Joystick
-  double TwistLimtFactor = -(m_DrvStickR.GetThrottle()-1.0)/2.0;  // Right Joystick
-
-
-  // Limit the drive motors for certain operations like color wheel
-  if (m_DrvStickL.GetRawButton(1)) // Break or well power off
-  {
-    m_drvL = 0.0;
-    m_drvR = 0.0;
-  }
-  else if (m_DrvStickL.GetRawButton(12))
-  {
-    CurrDriveConfig = CurrDriveConfig + 1;
-    if (CurrDriveConfig == LAST_ENTRY)
-        CurrDriveConfig = Std_Arcade;
-  }
-  else
-  {   
-    switch (CurrDriveConfig)
-    {
-    case Std_Arcade: // Single Joystick with No Twist
-        m_drvL = -RinY * PwrLimFactor;  // Speed
-        m_drvR =  RinX * PwrLimFactor;  // Turn
-        frc::SmartDashboard::PutString("DrvMode: ","Std Arcade       ");
-        break;
-    case Std_Tank: // Dual   Joystick with No Twist
-        m_drvL = -LinY * PwrLimFactor;  // Left Power
-        m_drvR = -RinY * PwrLimFactor;  // Right Power
-       frc::SmartDashboard::PutString("DrvMode: ","Std Tank         ");
-       break;
-    case Arcade_Twist: // Single Joystick with    Twist and Turn
-        m_drvL = -RinY * PwrLimFactor;                            // Speed
-        m_drvR = (RinX * PwrLimFactor) + (RinT*TwistLimtFactor);  // Turn
-        frc::SmartDashboard::PutString("DrvMode: ","Arcade with Twist");
-        break;
-    case Dual_Arc_Twist: // Dual   Joystick with    Twist
-        m_drvL = -LinY * PwrLimFactor;                            // Speed
-        m_drvR = (RinX * PwrLimFactor) + (RinT*TwistLimtFactor);  // Turn
-        frc::SmartDashboard::PutString("DrvMode: ","Dual Arcade Twist");
-        break;
-    case Arcade_Twist_Only: // Single Joystick with    Twist only
-        m_drvL = -RinY * PwrLimFactor;    // Speed
-        m_drvR = (RinT*TwistLimtFactor);  // Turn
-        frc::SmartDashboard::PutString("DrvMode: ","Arcade Twist Only");
-        break;
-    case ATO_LinTurn_Limit: // Single Joystick with    Twist only and limit the turn
-        m_drvL = -RinY * PwrLimFactor;             // Speed
-        curDrvL = -RinY;  // Grab the current speed request
-        if (fabs(curDrvL) < 0.5)   
-            VarTwistLim = 0.0;  // Don't do anything between 0..+/-0.5
-        else
-            VarTwistLim = curDrvL/2.0;  // Thake the power and scale it.  Keep the sign
-        // First remove some of the twist power based on FWD power and then
-        // scale the limit factor
-        m_drvR = limit(RinT-VarTwistLim)*TwistLimtFactor;  // Turn
-        frc::SmartDashboard::PutString("DrvMode: ","Arcade Twist Only");
-        break;
-    default:
-        m_drvL = 0.0;
-        m_drvR = 0.0;
-        frc::SmartDashboard::PutString("DrvMode: ","ERROR            ");
-        break;
-    }
-  }
-
-   // Drive with arcade style (use right stick)
-   if (CurrDriveConfig == Std_Tank)
-       m_robotDrive.TankDrive(m_drvL, m_drvR);  // Left Pwr, Right Pwr
-   else
-       m_robotDrive.ArcadeDrive(m_drvL, m_drvR);  // Speed, Rotate
-
-#if R2JESU_TURNON_SMARTDASHBOARD
-  frc::SmartDashboard::PutNumber("CurrDrvCfg", CurrDriveConfig);
-  frc::SmartDashboard::PutNumber("DrvL", m_drvL);
-  frc::SmartDashboard::PutNumber("DrvR", m_drvR);
-  frc::SmartDashboard::PutNumber("PwrLimit", PwrLimFactor);
-  frc::SmartDashboard::PutNumber("Twist", m_drvR);
-  frc::SmartDashboard::PutNumber("TwistLimit", TwistLimtFactor);
-  frc::SmartDashboard::PutNumber("VarTwistLim", VarTwistLim);
-#endif
-}
-
 #ifndef RUNNING_FRC_TESTS
-int main() {
+int main()
+{
   return frc::StartRobot<Robot>();
 }
 #endif
--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -1,48 +1,141 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-#pragma once
-
-#include <string>
-
+// WPI Libs
+#include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/Encoder.h>
+
+// Live Window and SmartDashboard
+#include <frc/livewindow/LiveWindow.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 #include <frc/smartdashboard/SendableChooser.h>
-#include <frc/Joystick.h.>
-#include <frc/DriverStation.h>
-#include "AHRS.h"
 
 // Motor Controllers
 #include <frc/spark.h>
 #include <frc/PWMVictorSPX.h>
-#include <frc/drive/DifferentialDrive.h>
+#include "rev/SparkMax.h"
+#include <rev/CANSparkMax.h>
 
-using namespace frc;
+// Color Sensor
+#include <frc/util/color.h>
+#include "rev/ColorSensorV3.h"
+#include "rev/ColorMatch.h"
+#include <frc/NidecBrushless.h>
+#include "frc/DriverStation.h"
+
+//  Nav Sensor
+#include <ctre/Phoenix.h>
+
+// NavX
+#include "AHRS.h"
+
+// Pneumatics
+#include <frc/Compressor.h>
+#include <frc/Solenoid.h>
+
+// Camera
+#include <thread>
+#include <cameraserver/CameraServer.h>
+//#include <frc/TimedRobot.h>
+//#include <wpi/raw_ostream.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <GripPipeline.h>
+#include <cmath>
+
+//Limit Switches
+#include <frc/DigitalInput.h>
+#include <frc/Timer.h>
+
+//Limelight 
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
 
 #define R2JESU_TURNON_SMARTDASHBOARD 1
 
-class Robot : public frc::TimedRobot {
- public:
-  void RobotInit() override;
-  void RobotPeriodic() override;
-  void AutonomousInit() override;
-  void AutonomousPeriodic() override;
-  void TeleopInit() override;
-  void TeleopPeriodic() override;
-  void DisabledInit() override;
-  void DisabledPeriodic() override;
-  void TestInit() override;
-  void TestPeriodic() override;
+// Control robot config for either Fin (1) or Rex (0)
+#define R2JESU_FIN_CONFIG 1
 
- private:
+#if R2JESU_FIN_CONFIG
+#define R2JESU_TURNON_PNEUMATICS 1
+#define R2JESU_TURNON_ENCODER 1
+#define R2JESU_TURNON_INTAKE 1
+#define R2JESU_TURNON_SHOOTER 1
+#define R2JESU_TURNON_WINCH 1
+#define R2JESU_TURNON_VISION 1
+#define R2JESU_TURNON_NAV 1
+#else
+#define R2JESU_TURNON_PNEUMATICS 0
+#define R2JESU_TURNON_ENCODER 0
+#define R2JESU_TURNON_INTAKE 0
+#define R2JESU_TURNON_SHOOTER 0
+#define R2JESU_TURNON_WINCH 0
+#define R2JESU_TURNON_VISION 1
+#define R2JESU_TURNON_NAV 1
+#endif
+
+class Robot : public frc::TimedRobot
+{
+public:
+  // Consturctor
+  //Robot();
+
+  // =================================================
+  //  Top Level Robot Functions
+  // =================================================
+
+  void RobotInit();
+
+  void RobotPeriodic();
+
+  void AutonomousInit();
+
+  void AutonomousPeriodic();
+
+  void TeleopInit();
+
+  void TeleopPeriodic();
+
+  void TestPeriodic();
+
+
+private:
   // =================================================
   //  Subsystem Control Functions
   // =================================================
 
   //  Main Robot Drive
- void R2Jesu_ProcessDrive(double p_LimitFactor = 1.0);
+  void R2Jesu_ProcessDrive(double p_LimitFactor = 1.0);
 
- double limit(double inVal, bool PosOnly = false)
+  //  Main User Control functions.
+  //  This may need to be split out.
+  void R2Jesu_ProcessUserControl(void);
+
+  // Shooter
+  void R2Jesu_ProcessShooter(void);
+
+  // Process the Color Wheel Control
+  void R2Jesu_ProcessColorWheel(void);
+  void R2Jesu_CheckGameTargetColor(void);
+  frc::Color R2Jesu_ReadColorWheel(void);
+
+  //Intake Processing
+  void R2Jesu_ProcessIntake(void);
+
+  // Vision Processing
+  static void VisionThread(void);
+
+  // Limit either between -1.0..1.0 or 0.0..1.0
+   double limit(double inVal, bool PosOnly = false)
  {
    double val = ((inVal>1.0)?1.0:((inVal<-1.0)?-1.0:inVal));
    
@@ -52,41 +145,89 @@ class Robot : public frc::TimedRobot {
    return(val);
  }
 
-  // Autonomous Test Code to control from Driver Station
-  frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+  // =================================================
+  //  Class Objects
+  // =================================================
 
-  // NavXP Sensor
-  AHRS *ahrs;
-
-  // Drive Configurations
-  typedef enum {
-    Std_Arcade        = 0, // Single Joystick with No Twist
-    Std_Tank          = 1, // Dual   Joystick with No Twist
-    Arcade_Twist      = 2, // Single Joystick with    Twist and Turn
-    Dual_Arc_Twist    = 3, // Dual   Joystick with    Twist
-    Arcade_Twist_Only = 4, // Single Joystick with    Twist only
-    ATO_LinTurn_Limit = 5, // Single Joystick with    Twist only and limit the turn
-    LAST_ENTRY        = 6
-  } DriveConfigType;
-   
-  unsigned int CurrDriveConfig = ATO_LinTurn_Limit;
-
-  // User Controllers
-  frc::Joystick m_DrvStickL{0};
-  frc::Joystick m_DrvStickR{1};
+  // User Control
+  frc::Joystick m_Drivestick{0};
+  frc::Joystick m_OperatorStick{1};
+  frc::Joystick m_TestStick{5};
 
   // Robot drive system
   frc::PWMVictorSPX m_leftMotor{0};  // Second motor wired to Y PWM Cablec
   frc::PWMVictorSPX m_rightMotor{1}; // Second motor wired to Y PWM Cable
   frc::DifferentialDrive m_robotDrive{m_leftMotor, m_rightMotor};
 
-  // Because motor control cmds may come from other parts of the code use these
-  // objects to control the final motor controldouble m_drvL;
-  double m_drvR;
-  double m_drvL;
+  // NavXP Sensor
+  #if R2JESU_TURNON_NAV
+  AHRS *ahrs;
+  #endif
 
-  int autoLoopCounter;
+// Encoders
+#if R2JESU_TURNON_ENCODER
+  frc::Encoder m_encL{7, 8, false, frc::Encoder::k4X};
+  frc::Encoder m_encR{4, 5, false, frc::Encoder::k4X};
+#endif
+
+  // Because motor control cmds may come from other parts of the code use these
+  // objects to control the final motor control
+  double m_drvL;
+  double m_drvR;
+
+  // Ball Intake Subsystem
+  WPI_TalonSRX snowMotor{2};
+
+// Ball Shooter Subsystem - Pneumatics & Motor
+#if R2JESU_TURNON_SHOOTER
+  rev::CANSparkMax m_ShooterMotorLeft{1, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_ShooterMotorRight{3, rev::CANSparkMax::MotorType::kBrushless};
+  frc::DigitalInput lowerLimit{1};
+  frc::DigitalInput ballCupLimit{2};
+#endif
+
+#if R2JESU_TURNON_PNEUMATICS
+  frc::Compressor compressorObject;
+  frc::Solenoid ballPopper{0};
+  frc::Solenoid colorArm{1};
+#endif
+
+
+  // Color Wheel Subsystem
+  static constexpr auto i2cPort = frc::I2C::Port::kOnboard;
+  rev::ColorSensorV3 m_colorSensor{i2cPort};
+  rev::ColorMatch m_colorMatcher;
+  static constexpr frc::Color kBlueTarget = frc::Color(0.204, 0.461, 0.335);
+  static constexpr frc::Color kGreenTarget = frc::Color(0.233, 0.516, 0.251);
+  static constexpr frc::Color kRedTarget = frc::Color(0.338, 0.447, 0.214);
+  static constexpr frc::Color kYellowTarget = frc::Color(0.286, 0.528, 0.186);
+  static constexpr frc::Color nun = frc::Color(0, 0, 0);
+  static constexpr frc::Color Default = frc::Color(1, 1, 1);
+  double ColorConfidence = 0.0;
+
+  std::string gameData;
+  frc::Color gameColor = nun;
+  const double NidecValue = 0.5;
+  frc::NidecBrushless ColorWheelmotor = frc::NidecBrushless(3, 0);
+
+  // Winch Subsystem
+  WPI_TalonSRX m_winchMotor{4};
+
+  // Vision System
+  static double turning;
+  static double currentDistance;
+
+  // Support Objects
+  frc::Timer m_timer;
+
+  //  Driver station Control
+  frc::SendableChooser<std::string> m_chooser;
+  const std::string kAutoNameDefault = "Default";
+  const std::string kAutoNameCustom = "My Auto";
+  std::string m_autoSelected;
+
+  // Debug & feedback
+  frc::LiveWindow &m_lw = *frc::LiveWindow::GetInstance();
+
+  std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 };
